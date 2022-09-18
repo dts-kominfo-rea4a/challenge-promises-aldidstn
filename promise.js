9ab30e56a32c00aafb77e42df5caf49092d20634
McAldi <mcaldi@McAldis-Mac-mini.local>
2022-09-18 19:05:12 +0700

    create promiseOutput function

--- promise.js
+++ promise.js
@@ -1,7 +1,27 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  
+  const theater1 = await promiseTheaterIXX()
+  const theater2 = await promiseTheaterVGC()
+
+  let arrayResult1 = theater1.map(key => key.hasil)
+  let arrayResult2 = theater2.map(key => key.hasil)
+  let result = arrayResult1.concat(arrayResult2)
+
+  let total = 0
+  
+  result.forEach(el => {
+    if(el === emosi){
+      total++
+    }
+  });
+
+  return total
+}
+
+promiseOutput()
 
 module.exports = {
   promiseOutput,
